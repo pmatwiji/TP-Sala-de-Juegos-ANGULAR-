f9dd9b12f401003b4a77f24f9a13fffb530f824d
pmatwiji <pmatwiji@gmail.com>
2020-09-03 20:53:29 -0300

    update 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AdivinaElNumeroComponent } from './componentes/adivina-el-numero/adivin
 import { ListadoDeResultadosComponent } from './componentes/listado-de-resultados/listado-de-resultados.component';
 import { LoginComponent } from './componentes/login/login.component';
 //  import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { HttpModule } from '@angular/http';
+//import { HttpModule } from '@angular/http';
 
 // import { AccordionModule } from 'ngx-bootstrap';
 // agrego las clases para utilizar ruteo
@@ -84,7 +84,6 @@ import { SexoPipe } from './pipes/sexo.pipe';
     BrowserModule,
     FormsModule,
     RuteandoModule,
-    HttpModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyB6f8x4IjRlesQ3oETc6BXYQHVRTOlY3Ys'
     })
--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 import {Subscription} from "rxjs";
-import {TimerObservable} from "rxjs/observable/TimerObservable";
+//import {TimerObservable} from "rxjs/observable/TimerObservable";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -36,7 +36,7 @@ export class LoginComponent implements OnInit {
       this.router.navigate(['/Principal']);
     }
   }
-  MoverBarraDeProgreso() {
+  /*MoverBarraDeProgreso() {
     
     this.logeando=false;
     this.clase="progress-bar progress-bar-danger progress-bar-striped active";
@@ -77,5 +77,5 @@ export class LoginComponent implements OnInit {
     });
     //this.logeando=true;
   }
-
+*/
 }
