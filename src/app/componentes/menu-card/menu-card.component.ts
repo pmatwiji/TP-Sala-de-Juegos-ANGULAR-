17bc71faa282a7ee77a23b9ab31c3e61b2405633
pmatwiji <pmatwiji@gmail.com>
2020-09-10 15:16:10 -0300

    cambie imagenes, sigo con el diseño

--- src/app/componentes/menu-card/menu-card.component.ts
+++ src/app/componentes/menu-card/menu-card.component.ts
@@ -16,16 +16,16 @@ export class MenuCardComponent implements OnInit {
   Juego(tipo: string) {
     switch (tipo) {
       case 'Adivina':
-          this.router.navigate(['/Juegos/Adivina']);
+          this.router.navigate(['/juegos/Adivina']);
         break;
       case 'Agilidad':
-          this.router.navigate(['/Juegos/Agilidad']);
+          this.router.navigate(['/juegos/Agilidad']);
         break;
       case 'AdivinaMasListado':
-          this.router.navigate(['/Juegos/AdivinaMasListado']);
+          this.router.navigate(['/juegos/AdivinaMasListado']);
         break;
       case 'AgilidadaMasListado':
-          this.router.navigate(['/Juegos/AgilidadaMasListado']);
+          this.router.navigate(['/juegos/AgilidadaMasListado']);
         break;
     }
   }
--- src/app/servicios/juego-service.service.ts
+++ src/app/servicios/juego-service.service.ts
@@ -9,7 +9,7 @@ export class JuegoServiceService {
   peticion:any;
   constructor( public miHttp: MiHttpService ) {
     this.peticion = this.miHttp.httpGetO("http://localhost:3003");
-//    this.peticion = this.miHttp.httpGetO("https://restcountries.eu/rest/v2/all");
+    //this.peticion = this.miHttp.httpGetO("https://restcountries.eu/rest/v2/all");
   }
 
   public listar(): Array<Juego> {
