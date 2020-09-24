eab13d8517bce85c1073bd24011e3261ff062ea6
pmatwiji <pmatwiji@gmail.com>
2020-09-24 18:14:30 -0300

    ...

--- src/app/componentes/anagrama/anagrama.component.ts
+++ src/app/componentes/anagrama/anagrama.component.ts
@@ -12,4 +12,46 @@ export class AnagramaComponent implements OnInit {
   ngOnInit() {
   }
 
+  palabras=['ficha','boton','palanca','moneda','pantalla','peluche','premio','ganador','perdedor','trofeo'];
+  palabraSeleccionada:string = "";
+  palabraSeparada;
+  palabraDesordenada;
+  juegoNuevo:boolean = false;
+  respuesta: string;
+  ganador: boolean = false;
+  perdedor:boolean = false;
+
+  nuevaPalabra(){
+    this.juegoNuevo=true;
+    this.palabraSeleccionada=this.palabras[Math.floor(Math.random() * (10 - 0))+ 0];
+    this.palabraSeparada = this.palabraSeleccionada.split('');
+    this.palabraDesordenada = this.palabraSeparada.sort(function() {
+      return Math.random() -0.5      
+    })
+
+
+  }
+
+  verificar(){
+    document.querySelector('#enviar').setAttribute('disabled','true');
+    if(this.palabraSeleccionada == this.respuesta.toLocaleLowerCase()){
+      this.ganador= true;
+    }else{
+      this.perdedor = true;
+    }
+
+    setTimeout(() => {
+      this.reset();
+    }, 3000);
+  }
+
+  reset(){
+    this.palabraSeleccionada= "";
+    this.juegoNuevo = false;
+    this.ganador = false;
+    this.perdedor = false;
+    this.respuesta= "";
+    document.querySelector('#enviar').removeAttribute('disabled');
+  }
+
 }
